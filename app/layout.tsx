a26fa0b408c4a3909ec1aef363ced0a78707501d
nicolas <nicolasfrancone2003@gmail.com>
2024-03-09 15:31:01 -0300

    Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`} style={{ backgroundImage: "url('/your-image-url.jpg')" }}>
+      <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden">
-          <Header />
+          <Header 
+          />
           {children}
         </div>
       </body>
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,54 +1,68 @@
 import NavMobile from './hamburger-mobile';
 import Banner from '@/components/banner'
+import Script from 'next/script'
 
 export default function Header() {
   return (
     <>
-    
-    <header className="absolute w-full z-30">
-    <Banner /> 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="flex items-center justify-between h-20">
-          
 
-          {/* Desktop navigation */}
-          <nav className="hidden md:flex md:grow">
-            {/* Desktop nav links */}
-            <ul className="flex-grow flex justify-center items-center">
-              <li>
-                <a href="/#features" className=" font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
-                  Features
-                </a>
-              </li>
-              <li>
-                <a href="/#demos" className=" font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
-                  Demos
-                </a>
-              </li>
-              <li>
-                <a href="/#benefits" className=" font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
-                  Benefits
-                </a>
-              </li>
-              <li>
-                <a href="/#testimonials" className="font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
-                  Testimonials
-                </a>
-              </li>
-              <li>
-                <a href="/#about" className=" font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
-                  About
-                </a>
-              </li>
-            </ul>
-          </nav>
+      <header className="absolute w-full z-30">
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
 
-          <NavMobile/>
+        <Script id="google-analytics">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+  `}
+        </Script>
+        <Banner />
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="flex items-center justify-between h-20">
 
+
+            {/* Desktop navigation */}
+            <nav className="hidden md:flex md:grow">
+              {/* Desktop nav links */}
+              <ul className="flex-grow flex justify-center items-center">
+                <li>
+                  <a href="/#features" className=" font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
+                    Features
+                  </a>
+                </li>
+                <li>
+                  <a href="/#demos" className=" font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
+                    Demos
+                  </a>
+                </li>
+                <li>
+                  <a href="/#benefits" className=" font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
+                    Benefits
+                  </a>
+                </li>
+                <li>
+                  <a href="/#testimonials" className="font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
+                    Testimonials
+                  </a>
+                </li>
+                <li>
+                  <a href="/#about" className=" font-bold text-xl font-medium text-custom-blue hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </nav>
+
+            <NavMobile />
+
+          </div>
         </div>
-      </div>
-    </header>
-    
+      </header>
+
     </>
   )
 }
