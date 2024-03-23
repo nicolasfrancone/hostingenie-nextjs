b2569c150b0210147b98d1a1fc915001341fae7e
nicolas <nicolasfrancone2003@gmail.com>
2024-03-23 16:32:23 -0300

    Feature: add links

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -29,21 +29,21 @@ export default function Footer() {
             {/* Social links */}
             <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
               <li>
-                <Link href="" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-custom-dark rounded-full transition duration-150 ease-in-out" aria-label="Twitter" style={{color:'#ccfefb' }}>
+                <Link href="https://twitter.com/hostingenie" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-custom-dark rounded-full transition duration-150 ease-in-out" aria-label="Twitter" style={{color:'#ccfefb' }}>
                   <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                     <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
                   </svg>
                 </Link>
               </li>
               <li className="ml-4">
-                <Link href="" className="flex justify-center items-center bg-gray-800 hover:text-gray-100 hover:bg-custom-dark rounded-full transition duration-150 ease-in-out" aria-label="Facebook" style={{color:'#ccfefb' }}>
+                <Link href="https://www.linkedin.com/company/hostingenie/" className="flex justify-center items-center bg-gray-800 hover:text-gray-100 hover:bg-custom-dark rounded-full transition duration-150 ease-in-out" aria-label="Facebook" style={{color:'#ccfefb' }}>
                   <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                     <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
                   </svg>
                 </Link>
               </li>
               <li className="ml-4">
-                <Link href="" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-custom-dark rounded-full transition duration-150 ease-in-out" aria-label="Instagram" style={{color:'#ccfefb' }}>
+                <Link href="https://www.linkedin.com/company/hostingenie/" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-custom-dark rounded-full transition duration-150 ease-in-out" aria-label="Instagram" style={{color:'#ccfefb' }}>
                   <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="20.145" cy="11.892" r="1" />
                     <path d="M16 20c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm0-6c-1.103 0-2 .897-2 2s.897 2 2 2 2-.897 2-2-.897-2-2-2z" />
