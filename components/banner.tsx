f98d28d648431cd605c4443d1baab603ffb70de1
nicolas <nicolasfrancone2003@gmail.com>
2024-04-18 16:14:28 -0300

    Feature: add FREE

--- components/banner.tsx
+++ components/banner.tsx
@@ -5,7 +5,7 @@ export default function Banner() {
     return (
         <div className="text-white text-center py-4 mx custom-gradient">
             <p className="text-lg font-bold mx-3">
-            ⚡BREAKING NEWS! Our first customers will have access to Hostingenie at <strong>COST PRICE</strong> for the initial months as a token of appreciation for trusting us!
+            ⚡BREAKING NEWS! Our first customers will have access to Hostingenie for <strong>FREE</strong> during the initial months as a token of our appreciation for trusting us!
             </p>
         </div>
     );
