aee0e5d6f00404f00370f10e7a500bc8c09398de
nicolas <nicolasfrancone2003@gmail.com>
2024-06-06 11:46:37 -0300

    Feature: add mg

--- components/banner.tsx
+++ components/banner.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 
 export default function Banner() {
     return (
-        <div className="text-white text-center py-4 mx custom-gradient">
+        <div className="text-white text-center py-2 mx custom-gradient">
             <p className="text-lg font-bold mx-3">
             ⚡BREAKING NEWS! Our first customers will have <strong>FREE ACCESS</strong> to Hostingenie for their first month!
             </p>
