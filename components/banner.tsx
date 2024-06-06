b1496d6fbb34ea174a766867d8ce11e0e267976c
nicolas <nicolasfrancone2003@gmail.com>
2024-06-06 11:22:09 -0300

    Feature: add lg

--- components/banner.tsx
+++ components/banner.tsx
@@ -5,7 +5,7 @@ export default function Banner() {
     return (
         <div className="text-white text-center py-4 mx custom-gradient">
             <p className="text-lg font-bold mx-3">
-            ⚡BREAKING NEWS! Our first customers will have access to Hostingenie for <strong>FREE</strong> during the initial months as a token of our appreciation for trusting us!
+            ⚡BREAKING NEWS! Our first customers will have <strong>FREE ACCESS</strong> to Hostingenie for their first month!
             </p>
         </div>
     );
