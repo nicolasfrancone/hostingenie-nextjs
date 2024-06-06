f88f8ab8d7a1badb6116ad73ab725221ab18f126
nicolas <nicolasfrancone2003@gmail.com>
2024-06-06 17:14:17 -0300

    Feature: add base

--- components/banner.tsx
+++ components/banner.tsx
@@ -4,7 +4,7 @@ import './styles.css';
 export default function Banner() {
     return (
         <div className="text-white text-center py-2 mx custom-gradient">
-            <p className="text-lg font-bold mx-3">
+            <p className="text-base font-bold mx-3">
             ⚡BREAKING NEWS! Our first customers will have <strong>FREE ACCESS</strong> to Hostingenie for their first month!
             </p>
         </div>
